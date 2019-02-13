e82874dbc4869531b930fc1b44fa65b91cd3dafd
pierre-gazaix <pierre.gazaix@etu.umontpellier.fr>
2019-02-13 15:53:36 +0100

    TP3Exo1

---
+++ src/main/java/fr/umontpellier/iut/exo2/Departement.java
@@ -0,0 +1,37 @@
+package fr.umontpellier.iut.exo2;
+
+import fr.umontpellier.iut.exo1.Etudiants;
+
+import java.util.ArrayList;
+
+public class Departement {
+
+    private String spécialité;
+    private String adresse;
+    private ArrayList<Etudiants> listeetud;
+
+    public Departement(String adresse, String spécialité) {
+        this.adresse = adresse;
+        this.spécialité = spécialité;
+        this.listeetud = new ArrayList<Etudiants>();
+    }
+
+    public String toString() {
+        String s = "";
+        s += "------------  [ " + this.spécialité + " - " + this.adresse + " ]  ------------ \n";
+        for(int i = 0; i < this.listeetud.size(); i++){
+            s += i+1  + "-" + this.listeetud.get(i).toString() + "\n";
+        }
+        return s;
+    }
+
+    public void inscrire(Etudiants etu){
+        this.listeetud.add(etu);
+    }
+
+    public void desinscrire(Etudiants etu){
+        this.listeetud.remove(etu);
+    }
+
+
+}
--- src/main/java/fr/umontpellier/iut/exo2/GestionEtudiants.java
+++ src/main/java/fr/umontpellier/iut/exo2/GestionEtudiants.java
@@ -1,7 +1,30 @@
 package fr.umontpellier.iut.exo2;
 
+import fr.umontpellier.iut.exo1.Departement;
+import fr.umontpellier.iut.exo1.Etudiants;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+
 public class GestionEtudiants {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented");
+        //throw new RuntimeException("Not yet implemented");
+
+        LocalDate dateNaissance = LocalDate.of(2000, Month.FEBRUARY, 27);
+        Etudiants lolo = new Etudiants("Gazaix", "Pierre", dateNaissance, "gazaixpierre@gmail.com", "10 avenue d'Unterschleissheim");
+        Etudiants toto = lolo;
+        Etudiants roro = new Etudiants("Roro", "Pierre", dateNaissance, "gazaixpierre@gmail.com", "10 avenue d'Unterschleissheim");
+        Etudiants momo = new Etudiants("Momo", "Pierre", dateNaissance, "gazaixpierre@gmail.com", "10 avenue d'Unterschleissheim");
+        Departement monDepInfo = new Departement("A l'IUT", "Informatique");
+
+        monDepInfo.inscrire(lolo);
+        monDepInfo.inscrire(toto);
+        monDepInfo.inscrire(roro);
+        monDepInfo.inscrire(momo);
+        monDepInfo.desinscrire(toto);
+
+        System.out.println(monDepInfo.toString());
+
     }
 }
